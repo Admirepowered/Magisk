70f77acc9c080fc0dc9d6d38a3b5c1ded5d7e173
Admirepowered <admire1971@outlook.com>
2026-02-06 01:06:39 +0800

    debug info

--- native/src/boot/bootimg.cpp
+++ native/src/boot/bootimg.cpp
@@ -976,19 +976,19 @@ void repack(Utf8CStr src_img, Utf8CStr out_img, bool skip_comp) {
         fprintf(stderr, "Updating boot image checksum (%s)...\n",
                 boot.flags[SHA256_FLAG] ? "SHA1" : "SHA256");
         uint32_t size = hdr->kernel_size();
-        fprintf(stderr, "%-*s [%u],offset=%lu\n", PADDING, "KERNEL_SZ", size, off.kernel);
+        fprintf(stderr, "%-*s [%u],offset=%u\n", PADDING, "KERNEL_SZ", size, off.kernel);
         ctx->update(byte_view(out.data() + off.kernel, size));
         ctx->update(byte_view(&size, sizeof(size)));
         size = hdr->ramdisk_size();
-        fprintf(stderr, "%-*s [%u],offset=%lu\n", PADDING, "RAMDISK_SZ", size, off.ramdisk);
+        fprintf(stderr, "%-*s [%u],offset=%u\n", PADDING, "RAMDISK_SZ", size, off.ramdisk);
         ctx->update(byte_view(out.data() + off.ramdisk, size));
         ctx->update(byte_view(&size, sizeof(size)));
         size = hdr->second_size();
-        fprintf(stderr, "%-*s [%u],offset=%lu\n", PADDING, "SECOND_SZ", size, off.second);
+        fprintf(stderr, "%-*s [%u],offset=%u\n", PADDING, "SECOND_SZ", size, off.second);
         ctx->update(byte_view(out.data() + off.second, size));
         ctx->update(byte_view(&size, sizeof(size)));
         size = hdr->extra_size();
-        fprintf(stderr, "%-*s [%u],offset=%lu\n", PADDING, "EXTRA_SZ", size, off.extra);
+        fprintf(stderr, "%-*s [%u],offset=%u\n", PADDING, "EXTRA_SZ", size, off.extra);
         if (size) {
             ctx->update(byte_view(out.data() + off.extra, size));
             ctx->update(byte_view(&size, sizeof(size)));
@@ -996,13 +996,13 @@ void repack(Utf8CStr src_img, Utf8CStr out_img, bool skip_comp) {
         uint32_t ver = hdr->header_version();
         if (ver == 1 || ver == 2) {
             size = hdr->recovery_dtbo_size();
-            fprintf(stderr, "%-*s [%u],offset=%lu\n", PADDING, "RECOVERY_DTBO_SZ", size, off.recovery_dtbo);
+            fprintf(stderr, "%-*s [%u],offset=%u\n", PADDING, "RECOVERY_DTBO_SZ", size, hdr->recovery_dtbo_offset());
             ctx->update(byte_view(out.data() + hdr->recovery_dtbo_offset(), size));
             ctx->update(byte_view(&size, sizeof(size)));
         }
         if (ver == 2) {
             size = hdr->dtb_size();
-            fprintf(stderr, "%-*s [%u],offset=%lu\n", PADDING, "DTB_SZ", size, off.dtb);
+            fprintf(stderr, "%-*s [%u],offset=%u\n", PADDING, "DTB_SZ", size, off.dtb);
             ctx->update(byte_view(out.data() + off.dtb, size));
             ctx->update(byte_view(&size, sizeof(size)));
         }
